33ccea322dfd8e98e89f1f94b4d014bb74ae4886
FarouqKhaled17 <faroukkhaled45@gmail.com>
2025-12-16 01:33:48 +0200

    feat: add User and Poll entities and update CircleCI pipeline for build and test

---
+++ src/main/java/com/Mizan/Mizan/model/Poll.java
@@ -0,0 +1,26 @@
+package com.Mizan.Mizan.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@Entity
+@NoArgsConstructor
+public class Poll {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String question;
+
+    @ElementCollection
+    private List<String> options = new ArrayList<>();
+
+    @ElementCollection
+    private List<Long> votes = new ArrayList<>();
+}
---
+++ src/main/java/com/Mizan/Mizan/model/User.java
@@ -0,0 +1,22 @@
+package com.Mizan.Mizan.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String Id;
+    private String name;
+    private String email;
+    private String password;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    private java.util.List<String> roles;
+}
